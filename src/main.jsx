735783211dce2f1d4fa4210e666493c269fcf41d
pritamsahu99 <pritamsahuofficial@gmail.com>
2024-05-01 21:36:47 +0530

    main/update

--- src/main.jsx
+++ src/main.jsx
@@ -5,5 +5,7 @@ import './index.css'
 import 'remixicon/fonts/remixicon.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
     <App />
+   </React.StrictMode>
 )
